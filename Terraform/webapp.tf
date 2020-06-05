9b03c62cf733c703bcbf7d12dec197d202ad2a77
Mert Senel <mert.senel@sgtech.com.au>
2020-06-06 00:44:43 +1000

    upgrade azurerm provider

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,11 +1,11 @@
- terraform {
-  required_version = ">= 0.11" 
-
-provider "azurerm" {
-  version         = ">= 2.0"
+  provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "~>2.0.0"
   features {}
 }
 
+ terraform {
+  required_version = ">= 0.11" 
  backend "azurerm" {
   storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform"
