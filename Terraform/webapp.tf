81bb6976757b1546eb2ca429d8eebfd2d5b92cf8
Mert Senel <32119844+MertSenel@users.noreply.github.com>
2020-06-06 10:05:03 +1000

    Update webapp.tf (#6)
    * Update webapp.tf
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * upgrade azurerm provider
    
    Co-authored-by: Mert Senel <mert.senel@sgtech.com.au>

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,3 +1,9 @@
+  provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "~>2.0.0"
+  features {}
+}
+
  terraform {
   required_version = ">= 0.11" 
  backend "azurerm" {
